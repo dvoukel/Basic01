a8fc4ee6e89955cd0d3eca2087f08c9f7751d72c
dvoukel <dvoukel@nbg.gr>
2023-04-06 09:53:23 +0300

    git sout

--- src/main/java/gr/codehub/nbgtraining/jse/Main.java
+++ src/main/java/gr/codehub/nbgtraining/jse/Main.java
@@ -3,5 +3,6 @@ package gr.codehub.nbgtraining.jse;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("For git");
     }
 }
